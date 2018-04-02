2a356c73ff8bce81b2d749a244ce27527b596099
Christopher Agius <chrisagius@fastmail.com>
2018-04-02 23:53:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
 var playlist = new Object({['Jane Zhang']: 'Dust My Shoulders Off'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
